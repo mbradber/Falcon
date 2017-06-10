779f0ce9a11d9a9918c645d856cb21c8c504c170
Matt Bradberry <mcbradberry@gmail.com>
2017-06-10 13:17:56 -0700

    moving triangle

--- Falcon/main.cpp
+++ Falcon/main.cpp
@@ -1,148 +1,161 @@
-/******************************************************************************\
- | OpenGL 4 Example Code.                                                       |
- | Accompanies written series "Anton's OpenGL 4 Tutorials"                      |
- | Email: anton at antongerdelan dot net                                        |
- | First version 27 Jan 2014                                                    |
- | Copyright Dr Anton Gerdelan, Trinity College Dublin, Ireland.                |
- | See individual libraries for separate legal notices                          |
- |******************************************************************************|
- | "Hello Triangle". Just the basics.                                           |
- | If you're on Apple un-comment the version number code at the beginning. It   |
- | will give you the latest, even if you say 3.2!                               |
- | This uses the libraries GLEW and GLFW3 to start GL. Download and compile     |
- | these first. Linking them might be a pain, but you'll need to master this.   |
- \******************************************************************************/
+#define GLFW_INCLUDE_GLCOREARB
 
-#include <GL/glew.h>		/* include GLEW and new version of GL on Windows */
-#include <GLFW/glfw3.h> /* GLFW helper library */
+#include "gl_utils.h"		// utility functions discussed in earlier tutorials
+#include <GL/glew.h>		// include GLEW and new version of GL on Windows
+#include <GLFW/glfw3.h> // GLFW helper library
+#include <assert.h>
+#include <math.h>
+#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define GL_LOG_FILE "gl.log"
+
+// keep track of window size for things like the viewport and the mouse cursor
+int g_gl_width = 640;
+int g_gl_height = 480;
+GLFWwindow *g_window = NULL;
 
 int main() {
-    GLFWwindow *window = NULL;
-    const GLubyte *renderer;
-    const GLubyte *version;
-    GLuint vao;
-    GLuint vbo;
-    /* geometry to use. these are 3 xyz points (9 floats total) to make a triangle */
+    restart_gl_log();
+    // all the GLFW and GLEW start-up code is moved to here in gl_utils.cpp
+    start_gl();
+    // tell GL to only draw onto a pixel if the shape is closer to the viewer
+    glEnable( GL_DEPTH_TEST ); // enable depth-testing
+    glDepthFunc( GL_LESS );		 // depth-testing interprets a smaller value as "closer"
+    
+    /* OTHER STUFF GOES HERE NEXT */
     GLfloat points[] = { 0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f };
-    /* these are the strings of code for the shaders
-     the vertex shader positions each vertex point */
-    const char *vertex_shader = "#version 410\n"
-    "in vec3 vp;"
-    "void main () {"
-    "	gl_Position = vec4 (vp, 1.0);"
-    "}";
-    /* the fragment shader colours each fragment (pixel-sized area of the
-     triangle) */
-    const char *fragment_shader = "#version 410\n"
-    "out vec4 frag_colour;"
-    "void main () {"
-    "	frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
-    "}";
-    /* GL shader objects for vertex and fragment shader [components] */
-    GLuint vert_shader, frag_shader;
-    /* GL shader programme object [combined, to link] */
-    GLuint shader_programme;
     
-    /* start GL context and O/S window using the GLFW helper library */
-    if ( !glfwInit() ) {
-        fprintf( stderr, "ERROR: could not start GLFW3\n" );
-        return 1;
-    }
+    GLfloat colours[] = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
     
-    /* We must specify 3.2 core if on Apple OS X -- other O/S can specify
-     anything here. I defined 'APPLE' in the makefile for OS X
-     
-     Remove the #ifdef #endif and play around with this - you should be starting
-     an explicit version anyway, and some non-Apple drivers will require this too.
-     */
-#ifdef APPLE
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
-    glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
-    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
-#endif
-    
-    window = glfwCreateWindow( 640, 480, "Hello Triangle", NULL, NULL );
-    if ( !window ) {
-        fprintf( stderr, "ERROR: could not open window with GLFW3\n" );
-        glfwTerminate();
-        return 1;
-    }
-    glfwMakeContextCurrent( window );
-    /* start GLEW extension handler */
-    glewExperimental = GL_TRUE;
-    glewInit();
-    
-    /* get version info */
-    renderer = glGetString( GL_RENDERER ); /* get renderer string */
-    version = glGetString( GL_VERSION );	 /* version as a string */
-    printf( "Renderer: %s\n", renderer );
-    printf( "OpenGL version supported %s\n", version );
-    
-    /* tell GL to only draw onto a pixel if the shape is closer to the viewer
-     than anything already drawn at that pixel */
-    glEnable( GL_DEPTH_TEST ); /* enable depth-testing */
-    /* with LESS depth-testing interprets a smaller depth value as meaning "closer" */
-    glDepthFunc( GL_LESS );
-    /* a vertex buffer object (VBO) is created here. this stores an array of
-     data on the graphics adapter's memory. in our case - the vertex points */
-    glGenBuffers( 1, &vbo );
-    glBindBuffer( GL_ARRAY_BUFFER, vbo );
+    GLuint points_vbo;
+    glGenBuffers( 1, &points_vbo );
+    glBindBuffer( GL_ARRAY_BUFFER, points_vbo );
     glBufferData( GL_ARRAY_BUFFER, 9 * sizeof( GLfloat ), points, GL_STATIC_DRAW );
     
-    /* the vertex array object (VAO) is a little descriptor that defines which
-     data from vertex buffer objects should be used as input variables to vertex
-     shaders. in our case - use our only VBO, and say 'every three floats is a
-     variable' */
+    GLuint colours_vbo;
+    glGenBuffers( 1, &colours_vbo );
+    glBindBuffer( GL_ARRAY_BUFFER, colours_vbo );
+    glBufferData( GL_ARRAY_BUFFER, 9 * sizeof( GLfloat ), colours, GL_STATIC_DRAW );
+    
+    GLuint vao;
     glGenVertexArrays( 1, &vao );
     glBindVertexArray( vao );
-    // "attribute #0 should be enabled when this vao is bound"
-    glEnableVertexAttribArray( 0 );
-    // this VBO is already bound, but it's a good habit to explicitly specify which
-    // VBO's data the following
-    // vertex attribute pointer refers to
-    glBindBuffer( GL_ARRAY_BUFFER, vbo );
-    // "attribute #0 is created from every 3 variables in the above buffer, of type
-    // float (i.e. make me vec3s)"
+    glBindBuffer( GL_ARRAY_BUFFER, points_vbo );
     glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, NULL );
+    glBindBuffer( GL_ARRAY_BUFFER, colours_vbo );
+    glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, NULL );
+    glEnableVertexAttribArray( 0 );
+    glEnableVertexAttribArray( 1 );
     
-    /* here we copy the shader strings into GL shaders, and compile them. we
-     then create an executable shader 'program' and attach both of the compiled
-     shaders. we link this, which matches the outputs of the vertex shader to
-     the inputs of the fragment shader, etc. and it is then ready to use */
-    vert_shader = glCreateShader( GL_VERTEX_SHADER );
-    glShaderSource( vert_shader, 1, &vertex_shader, NULL );
-    glCompileShader( vert_shader );
-    frag_shader = glCreateShader( GL_FRAGMENT_SHADER );
-    glShaderSource( frag_shader, 1, &fragment_shader, NULL );
-    glCompileShader( frag_shader );
-    shader_programme = glCreateProgram();
-    glAttachShader( shader_programme, frag_shader );
-    glAttachShader( shader_programme, vert_shader );
-    glLinkProgram( shader_programme );
+    char vertex_shader[1024 * 256];
+    char fragment_shader[1024 * 256];
+    parse_file_into_str( "shaders/test_vs.glsl", vertex_shader, 1024 * 256 );
+    parse_file_into_str( "shaders/test_fs.glsl", fragment_shader, 1024 * 256 );
     
-    /* this loop clears the drawing surface, then draws the geometry described
-     by the VAO onto the drawing surface. we 'poll events' to see if the window
-     was closed, etc. finally, we 'swap the buffers' which displays our drawing
-     surface onto the view area. we use a double-buffering system which means
-     that we have a 'currently displayed' surface, and 'currently being drawn'
-     surface. hence the 'swap' idea. in a single-buffering system we would see
-     stuff being drawn one-after-the-other */
-    while ( !glfwWindowShouldClose( window ) ) {
-        /* wipe the drawing surface clear */
-        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-        glUseProgram( shader_programme );
-        glBindVertexArray( vao );
-        /* draw points 0-3 from the currently bound VAO with current in-use shader */
-        glDrawArrays( GL_TRIANGLES, 0, 3 );
-        /* update other events like input handling */
-        glfwPollEvents();
-        /* put the stuff we've been drawing onto the display */
-        glfwSwapBuffers( window );
+    GLuint vs = glCreateShader( GL_VERTEX_SHADER );
+    const GLchar *p = (const GLchar *)vertex_shader;
+    glShaderSource( vs, 1, &p, NULL );
+    glCompileShader( vs );
+    
+    // check for compile errors
+    int params = -1;
+    glGetShaderiv( vs, GL_COMPILE_STATUS, &params );
+    if ( GL_TRUE != params ) {
+        fprintf( stderr, "ERROR: GL shader index %i did not compile\n", vs );
+        print_shader_info_log( vs );
+        return 1; // or exit or something
     }
     
-    /* close GL context and any other GLFW resources */
+    GLuint fs = glCreateShader( GL_FRAGMENT_SHADER );
+    p = (const GLchar *)fragment_shader;
+    glShaderSource( fs, 1, &p, NULL );
+    glCompileShader( fs );
+    
+    // check for compile errors
+    glGetShaderiv( fs, GL_COMPILE_STATUS, &params );
+    if ( GL_TRUE != params ) {
+        fprintf( stderr, "ERROR: GL shader index %i did not compile\n", fs );
+        print_shader_info_log( fs );
+        return 1; // or exit or something
+    }
+    
+    GLuint shader_programme = glCreateProgram();
+    glAttachShader( shader_programme, fs );
+    glAttachShader( shader_programme, vs );
+    glLinkProgram( shader_programme );
+    
+    glGetProgramiv( shader_programme, GL_LINK_STATUS, &params );
+    if ( GL_TRUE != params ) {
+        fprintf( stderr, "ERROR: could not link shader programme GL index %i\n",
+                shader_programme );
+        print_programme_info_log( shader_programme );
+        return false;
+    }
+    
+    GLfloat matrix[] = {
+        1.0f, 0.0f, 0.0f, 0.0f, // first column
+        0.0f, 1.0f, 0.0f, 0.0f, // second column
+        0.0f, 0.0f, 1.0f, 0.0f, // third column
+        0.5f, 0.0f, 0.0f, 1.0f	// fourth column
+    };
+    
+    int matrix_location = glGetUniformLocation( shader_programme, "matrix" );
+    glUseProgram( shader_programme );
+    glUniformMatrix4fv( matrix_location, 1, GL_FALSE, matrix );
+    
+    glEnable( GL_CULL_FACE ); // cull face
+    glCullFace( GL_BACK );		// cull back face
+    glFrontFace( GL_CW );			// GL_CCW for counter clock-wise
+    
+    float speed = 1.0f; // move at 1 unit per second
+    float last_position = 0.0f;
+    while ( !glfwWindowShouldClose( g_window ) ) {
+        // add a timer for doing animation
+        static double previous_seconds = glfwGetTime();
+        double current_seconds = glfwGetTime();
+        double elapsed_seconds = current_seconds - previous_seconds;
+        previous_seconds = current_seconds;
+        
+        _update_fps_counter( g_window );
+        // wipe the drawing surface clear
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+        glViewport( 0, 0, g_gl_width, g_gl_height );
+        
+        //
+        // Note: this call is not necessary, but I like to do it anyway before any
+        // time that I call glDrawArrays() so I never use the wrong shader programme
+        glUseProgram( shader_programme );
+        
+        // update the matrix
+        // - you could simplify this by just using sin(current_seconds)
+        matrix[12] = elapsed_seconds * speed + last_position;
+        last_position = matrix[12];
+        if ( fabs( last_position ) > 1.0 ) {
+            speed = -speed;
+        }
+        //
+        // Note: this call is related to the most recently 'used' shader programme
+        glUniformMatrix4fv( matrix_location, 1, GL_FALSE, matrix );
+        
+        //
+        // Note: this call is not necessary, but I like to do it anyway before any
+        // time that I call glDrawArrays() so I never use the wrong vertex data
+        glBindVertexArray( vao );
+        // draw points 0-3 from the currently bound VAO with current in-use shader
+        glDrawArrays( GL_TRIANGLES, 0, 3 );
+        // update other events like input handling
+        glfwPollEvents();
+        if ( GLFW_PRESS == glfwGetKey( g_window, GLFW_KEY_ESCAPE ) ) {
+            glfwSetWindowShouldClose( g_window, 1 );
+        }
+        // put the stuff we've been drawing onto the display
+        glfwSwapBuffers( g_window );
+    }
+    
+    // close GL context and any other GLFW resources
     glfwTerminate();
     return 0;
 }
